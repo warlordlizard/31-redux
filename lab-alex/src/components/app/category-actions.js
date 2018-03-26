59ec0d263ef84c28699c1c450272a718b6369d34
warlordlizard <alexjbowers88@gmail.com>
2018-03-26 13:59:25 -0700

    action and reducer

---
+++ lab-alex/src/components/app/category-actions.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import uuid from 'uuid/v4';
+
+export const categoryCreate = (category) => {
+  category.id = uuid();
+  category.timestamp = new Date();
+  return {
+    type: 'CATEGORY_CREATE',
+    payload: category,
+  };
+};
+
+export const categoryUpdate = (category) => ({
+  type: 'CATEGORY_UPDATE',
+  payload: category,
+});
+export const categoryDelete = (category) => ({
+  type: 'CATEGORY_DELETE',
+  payload: category,
+});
